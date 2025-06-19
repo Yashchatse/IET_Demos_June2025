6647e868af86bc5a7521d6fe8baa6bfafcdf6070
Tech Mugdha <the.tech.mugdha@gmail.com>
2025-06-19 13:42:23 +0530

    Day 08 : Custom Attributes, Reflection, ORM Demo

---
+++ CSharpDemos25/31CMathLib/CMath.cs
@@ -0,0 +1,19 @@
+﻿using _35MyCustomAttribute;
+
+namespace _31CMathLib
+{
+    [Serializable]
+    [BVS(CompanyName ="BonaventureSystems",DeveloperName ="Mugdha")]
+    public class CMath
+    {
+        //[BVS(CompanyName = "BonaventureSystems", DeveloperName = "Mugdha")]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ CSharpDemos25/35MyCustomAttribute/BVS.cs
@@ -0,0 +1,22 @@
+﻿namespace _35MyCustomAttribute
+{
+	[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class BVS : Attribute
+    {
+		private string _CompanyName;
+		private string _DeveloperName;
+
+		public string DeveloperName
+		{
+			get { return _DeveloperName; }
+			set { _DeveloperName = value; }
+		}
+
+		public string CompanyName
+		{
+			get { return _CompanyName; }
+			set { _CompanyName = value; }
+		}
+
+	}
+}
